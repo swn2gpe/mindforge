ec0d6c6bc16c57e644037c83bae249ec02b912df
SWN <swannphibel9@gmail.com>
2026-03-12 16:43:20 -0400

    feat: ajout PWA - service worker, manifest, icones, cache API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 // Configuration principale de l'application Angular
 export const appConfig: ApplicationConfig = {
@@ -12,6 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),                                 // Active le routage avec nos routes
     provideHttpClient(withInterceptors([authInterceptor])), // Active HttpClient avec l'intercepteur JWT
-    provideAnimationsAsync()                                // Active les animations Angular (chargées en lazy)
+    provideAnimationsAsync(),                                // Active les animations Angular (chargées en lazy)
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
